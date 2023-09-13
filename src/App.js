781f4093f45858399cd4eafc6767ef66798c415e
ASiracusa <fedorafilmz@gmail.com>
2023-09-12 22:32:02 -0400

    Added Hello World Fetch

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 function App() {
+
+  const [backendData, setBackendData] = useState([{}]);
+
+  useEffect(() => {
+    fetch("/api/hello").then(
+      response => response.json()
+    ).then(
+      data => {
+        setBackendData(data);
+      }
+    )
+  }, []);
+
   return (
     <div>
-
+      <h3>{
+        (typeof backendData.hello === 'undefined') ?
+          "Loading..." : backendData.hello
+      }</h3>
     </div>
   )
 }
